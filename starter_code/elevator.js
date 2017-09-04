38c3637878a2880a87ee2a98b950500ed21d362c
Luisca108 <luiscarlos3106@gmail.com>
2017-09-04 19:02:31 +0200

    almost done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,59 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.waitingList = [];
+    this.passengers = [];
+    this.idInterval;
+    this.direction = "down";
+    this.destinations = []
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() { this.idInterval = setInterval(()=>{
+      this.update();
+    }, 3000);
+  }
+
+  stop() { clearInterval(this.idInterval); }
+
+  update() {this.log();}
+
+  _passengersEnter() {
+    if(this.waitingList.length > 0) {
+      this.waitingList.forEach((elem, index) => {
+      if (this.floor == elem.originFloor){
+        this.passengers.push(elem);
+        this.waitingList.splice(index, 1);
+      }
+    }
+
+  _passengersLeave() {
+    if(this.passengers.length > 0 ){
+     this.passengers.forEach((elem, index)=>{
+      if(elem.destinationFloor == this.floor){
+      this.passengers.splice(index, 1);
+    }  
+   }
+
+  floorUp() {
+    if(this.floor < this.MAXFLOOR  || this.waitingList.length > 0 || this.passengers.length > 0){
+      this.floor+=1;
+    }
+    console.log ("PISO: " + this.floor)
+  }
+
+  floorDown() {
+    if(this.floor > 0 || this.waitingList.length > 0 || this.passengers.length > 0){
+      this.floor--;
+    }
+    console.log ("PISO: " + this.floor)
+  }
+
+  call(Person) {
+    this.waitingList.push(Person);
+    this.requests.push(Person.originFloor);
+  }
+
+  log() {console.log(`Direction : ${this.direction} | Floor: ${this.floor}`);}
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,25 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let elev = new Elevator();
+
+elev.floorUp();
+elev.floorUp();
+elev.floorUp();
+elev.floorUp();
+elev.floorUp();
+elev.floorUp();
+elev.floorUp();
+elev.floorUp();
+elev.floorUp();
+elev.floorUp();
+
+elev.floorDown();
+elev.floorDown();
+elev.floorDown();
+elev.floorDown();
+elev.floorDown();
+elev.floorDown();
+elev.floorDown();
+elev.floorDown();
+elev.floorDown();
